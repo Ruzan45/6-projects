274970ce6e107f1e4b8ca4696519abf08405eed4
Ruzan45 <ruzan45@list.ru>
2025-08-25 16:37:13 +0500

    рабочий список фронденд

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,93 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const usrData = {
+    "page": 1,
+    "per_page": 5,
+    "total": 5,
+    "total_pages": 2,
+    "data": [
+      {
+        "id": 1,
+        "email": "email1.com",
+        "first_name": "Gergie",
+        "last_name": "Bluth",
+        "avatar": "https://avatars.mds.yandex.net/get-shedevrum/10502823/video_preview_fa08e39c047b11efa49e0a6d81cb79fd_2/orig"
+      },
+      {
+        "id": 2,
+        "email": "email2.com",
+        "first_name": "Kris",
+        "last_name": "Pussy",
+        "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwlIZbgZj8fzOa0PH5iGJL2w3p-EF5KB2rIw&s"
+      },
+      {
+        "id": 3,
+        "email": "email3.com",
+        "first_name": "Mark",
+        "last_name": "Member",
+        "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWgM7j_ZRxYfcwg79umk8X4e6n9CnAP_U0cg&s"
+      },
+      {
+        "id": 4,
+        "email": "email4.com",
+        "first_name": "Miss",
+        "last_name": "Doiki",
+        "avatar": "https://i.pinimg.com/474x/13/c2/55/13c255a0798b50f8a338446b78c64940.jpg"
+      },
+      {
+        "id": 5,
+        "email": "email5.com",
+        "first_name": "Pavel",
+        "last_name": "Durov",
+        "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS14ZgdxmFPJabR-Re7KmP36GQYowBAI6_-6Q&s"
+      }
+    ]
+  }
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([])
+  const [isLoading, setLoading] = React.useState(false);
+  const [isSucsess, setSucsess] = React.useState(false);
+
+  const [search, setSearch] = React.useState('')
+
+  /*   React.useEffect(() => {
+      fetch('https://reqres.in/api/users').then(res => res.json()).then(json => {
+        setUsers(json.data);
+      }).catch(err => {
+        console.log(err);
+        alert('Ошибка при получении пользователей')
+      }).finally(() => setLoading(false))
+    }, []) */
+  const onChangeSearchVal = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id))
+    } else {
+      setInvites(prev => [...prev, id]);
+    }
+  }
+  const onClickSucsess = (query) => {
+    if (query.length !== 0) {
+      setSucsess(true)
+    }
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {isSucsess === false ?
+        (<Users
+          onChangeSearchVal={onChangeSearchVal}
+          search={search} isLoading={isLoading}
+          setLoading={setLoading}
+          users={usrData}
+          onClickInvite={onClickInvite}
+          invites={invites}
+          onClickSucsess={onClickSucsess}
+        />) : (<Success />)}
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+
+
+export const Users = ({ users, isLoading, onChangeSearchVal, search, onClickInvite, invites, onClickSucsess }) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          onChange={onChangeSearchVal}
+          value={search}
+          type="text"
+          placeholder="Найти пользователя..." />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,10 +25,14 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {users.data.filter(obj => {
+            const fullName = (obj.first_name + ' ' + obj.last_name).toLowerCase();
+            return fullName.includes((search).toLowerCase())
+
+          }).map(obj => (<User key={obj.id} {...obj} isInvited={invites.includes(obj.id)} onClickInvite={onClickInvite} invites={invites} />))}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {invites.length > 0 && <button onClick={() => onClickSucsess(invites)} className="send-invite-btn">Отправить приглашение</button>}
     </>
   );
 };
